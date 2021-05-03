d164901abae2a53747500960329a3e1950eb3c92
Veer <viru0458u@gmail.com>
2021-05-03 18:17:04 +0530

    Binded ProductList into the grid

--- shopping-cart-redux/src/App.js
+++ shopping-cart-redux/src/App.js
@@ -1,13 +1,34 @@
+import React from "react";
 import "./App.css";
+import Product from "./components/Product";
+import data from "./data.json";
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header>React Shopping Cart</header>
-      <main>Product List</main>
-      <footer>All Rights Are Reserved</footer>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      sort: "",
+      size: "",
+    };
+  }
+
+  render() {
+    return (
+      <div className="grid-container">
+        <header>React Shopping Cart</header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Product products={this.state.products} />
+            </div>
+            <div className="sidebar">Cart</div>
+          </div>
+        </main>
+        <footer>All Rights Are Reserved</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
