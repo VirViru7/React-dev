8cf7f864efbd9752a9fe3b55b9d86b2ffb7915f8
Veer <viru0458u@gmail.com>
2021-05-03 22:16:54 +0530

    Added Filter for shopping cart

--- shopping-cart-redux/src/App.js
+++ shopping-cart-redux/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Filter from "./components/Filter";
 import Product from "./components/Product";
 import data from "./data.json";
 
@@ -13,6 +14,40 @@ class App extends React.Component {
     };
   }
 
+  handleFilterSort = (event) => {
+    const sort = event.target.value;
+
+    this.setState((state) => ({
+      sort: sort,
+      products: this.state.products.sort((a, b) =>
+        sort === "Lowest"
+          ? a.price > b.price
+            ? 1
+            : -1
+          : sort === "Highest"
+          ? a.price < b.price
+            ? 1
+            : -1
+          : a._id < b._id
+          ? 1
+          : -1
+      ),
+    }));
+  };
+
+  handleFilterSize = (event) => {
+    if (event.target.value === "") {
+      this.setState({ size: event.target.value, products: data.products });
+    } else {
+      this.setState({
+        size: event.target.value,
+        products: data.products.filter(
+          (product) => product.availableSizes.indexOf(event.target.value) >= 0
+        ),
+      });
+    }
+  };
+
   render() {
     return (
       <div className="grid-container">
@@ -20,6 +55,13 @@ class App extends React.Component {
         <main>
           <div className="content">
             <div className="main">
+              <Filter
+                count={this.state.products.length}
+                sort={this.state.sort}
+                size={this.state.size}
+                filterSort={(e) => this.handleFilterSort(e)}
+                filterSize={(e) => this.handleFilterSize(e)}
+              />
               <Product products={this.state.products} />
             </div>
             <div className="sidebar">Cart</div>
