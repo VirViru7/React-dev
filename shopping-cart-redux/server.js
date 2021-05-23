c0e45ea627632f48a4dc066e7e4d7ec0c8073ca9
Veer <viru0458u@gmail.com>
2021-05-23 19:38:56 +0530

    prepare to deploy

--- shopping-cart-redux/server.js
+++ shopping-cart-redux/server.js
@@ -99,5 +99,5 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
 }
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`serve at http://localhost:${port}`));
